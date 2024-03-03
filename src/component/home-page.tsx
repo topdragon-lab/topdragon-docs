913462eaaece503ed879a92fef8882b6128e3df5
LoggingCo <109440399+LoggingCo@users.noreply.github.com>
2024-03-04 07:55:46 +0900

    init

---
+++ src/component/home-page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const HomePage = () => {
+  return <div>시작하기</div>;
+};
---
+++ theme.config.jsx
@@ -0,0 +1,9 @@
+export default {
+  logo: <span>TD-LABS</span>,
+  project: {
+    link: "https://github.com/shuding/nextra",
+  },
+  nextThemes: {
+    defaultTheme: "dark",
+  },
+};
